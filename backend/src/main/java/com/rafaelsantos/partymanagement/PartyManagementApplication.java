f828ce204ca5fdeec70ff5298430313b3a8a4381
Rafael Santos <rafa.05125@gmail.com>
2022-04-27 14:34:02 -0300

    Project created

---
+++ backend/src/main/java/com/rafaelsantos/partymanagement/PartyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.partymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyManagementApplication.class, args);
+	}
+
+}
